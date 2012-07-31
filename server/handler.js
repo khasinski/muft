2de3b003b308e94876e9fc8d9f73bf155611611c
Krzysztof Hasiński <krzysztof.hasinski+muft@gmail.com>
2012-07-31 21:23:07 +0200

    stub files for node.js server

---
+++ server/handler.js
@@ -0,0 +1,12 @@
+var get = function (request) { 
+  console.log('get');
+  return 0;
+}
+
+var put = function (request) {
+  console.log('put');
+  return 0;
+}
+
+exports.put = put;
+exports.get = get;
---
+++ server/server.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var url = require('url');
+
+var handler = require('./handler');
+
+var handle = {};
+handle['/'] = handler.get;
+handle['/get'] = handler.get;
+handle['/put'] = handler.put;
+
+
+function onRequest(request, response) {
+  response.writeHead(200, {"Content-Type": "application/json"}); 
+  var pathname = url.parse(request.url).pathname;
+  if(undefined != handle[pathname]) {
+  	response.write(JSON.stringify(handle[pathname](request)));
+  }
+  else
+  	response.write(JSON.stringify({'error':'404'}));
+  response.end();
+}
+
+http.createServer(onRequest).listen(8888);
